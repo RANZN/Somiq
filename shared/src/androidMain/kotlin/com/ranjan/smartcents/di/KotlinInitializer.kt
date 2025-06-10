33a0ad0c5a37186eed0434690e91406aa27a8d41
RANZN <aryaprakashranjan@gmail.com>
2025-06-10 09:08:50 +0530

    Refactor: Set up Koin and Firebase
    This commit introduces Koin for dependency injection and initializes Firebase.
    
    Key changes:
    - Added `sharedModules` in Koin to provide dependencies for `AuthRepo` and `QuizRepo`.
    - Configured Firebase Firestore collections for "users" and "questions".
    - Initialized FirebaseApp with project-specific options in `KotlinInitializer`.

--- shared/src/androidMain/kotlin/com/ranjan/smartcents/di/KotlinInitializer.kt
+++ shared/src/androidMain/kotlin/com/ranjan/smartcents/di/KotlinInitializer.kt
@@ -1,11 +1,19 @@
 package com.ranjan.smartcents.di
 
 import android.content.Context
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 
 actual class KotlinInitializer(private val context: Context) {
     actual fun init() {
+        val options = FirebaseOptions.Builder()
+            .setProjectId("we4rule-1f526")
+            .setApplicationId("1:477460285859:android:65c6e012419365c238f216") // From google-services.json -> client -> client_info -> mobilesdk_app_id
+            .setApiKey("AIzaSyB4q-Tg24TamJBsI6VQLBDuEn5x8jD4WLA") // From google-services.json -> client -> api_key -> current_key
+            .build()
+        FirebaseApp.initializeApp(context, options)
         initKoin {
             androidContext(context)
             androidLogger()
--- shared/src/commonMain/kotlin/com/ranjan/smartcents/di/SharedModules.kt
+++ shared/src/commonMain/kotlin/com/ranjan/smartcents/di/SharedModules.kt
@@ -1,7 +1,33 @@
 package com.ranjan.smartcents.di
 
+import com.ranjan.smartcents.data.repository.AuthRepoImpl
+import com.ranjan.smartcents.data.repository.QuizRepoImpl
+import com.ranjan.smartcents.domain.repository.AuthRepo
+import com.ranjan.smartcents.domain.repository.QuizRepo
+import dev.gitlive.firebase.Firebase
+import dev.gitlive.firebase.firestore.firestore
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val sharedModules = module {
 
+    single(named("userDatabase")) {
+        Firebase.firestore.collection("users")
+    }
+
+    factory<AuthRepo> {
+        AuthRepoImpl(
+            userDatabase = get(named("userDatabase"))
+        )
+    }
+
+    single(named("questionDatabase")) {
+        Firebase.firestore.collection("questions")
+    }
+
+    factory<QuizRepo> {
+        QuizRepoImpl(
+            questionDatabase = get(named("questionDatabase"))
+        )
+    }
 }

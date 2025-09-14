af3c8c926a54a857c6778619fb262ca71e11a569
RANZN <aryaprakashranjan@gmail.com>
2025-09-14 13:20:38 +0530

    Refactor: Update App Theme and Edge-to-Edge Display
    This commit updates the application's theme and enables edge-to-edge display in `MainActivity`.
    
    Key changes:
    - Modified `MainActivity` to use `enableEdgeToEdge` with a yellow system bar style.
    - Wrapped `AppNavigation` within a `Scaffold` in the `App` composable to utilize `innerPadding` for proper layout within the new edge-to-edge display.

--- composeApp/src/androidMain/kotlin/com/ranjan/smartcents/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/ranjan/smartcents/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.ranjan.smartcents
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
@@ -9,7 +11,7 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
+        enableEdgeToEdge(SystemBarStyle.auto(Color.YELLOW, Color.YELLOW))
         super.onCreate(savedInstanceState)
 
         setContent {
--- composeApp/src/commonMain/kotlin/com/ranjan/smartcents/App.kt
+++ composeApp/src/commonMain/kotlin/com/ranjan/smartcents/App.kt
@@ -1,11 +1,16 @@
 package com.ranjan.smartcents
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.ranjan.smartcents.navigation.AppNavigation
 
 @Composable
 fun App() {
     MyApplicationTheme {
-        AppNavigation()
+        Scaffold { innerPadding ->
+            AppNavigation(Modifier.padding(innerPadding))
+        }
     }
 }
